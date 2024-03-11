b40b15125ead0cde4833b51d788e789af0a0b3a8
Paul Duan <paul.duan@gmail.com>
2024-03-11 20:49:39 +1100

    modify test.ps1

--- test.ps1
+++ test.ps1
@@ -1,13 +1,13 @@
 
 
 param (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$StorageAccountName,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$StorageAccountKey,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$ShareName
 )
 
@@ -31,3 +31,109 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 $VHDPath | Out-File -FilePath C:\test.txt -Append
 $StorageAccountKey | Out-File -FilePath C:\test.txt -Append
 $RegPath | Out-File -FilePath C:\test.txt -Append
+
+cmdkey.exe /add:$StorageAccountName.file.core.windows.net /user:localhost\$StorageAccountName /pass:$StorageAccountKey
+
+
+function Write-Log {
+    param(
+            [parameter(Mandatory)]
+            [string]$Message,
+
+            [parameter(Mandatory)]
+            [string]$Type
+    )
+    $Path = 'C:\Windows\Temp\AVDSessionHostConfig.log'
+    if (!(Test-Path -Path $Path)) {
+            New-Item -Path 'C:\' -Name 'AVDSessionHostConfig.log' | Out-Null
+    }
+    $Timestamp = Get-Date -Format 'MM/dd/yyyy HH:mm:ss.ff'
+    $Entry = '[' + $Timestamp + '] [' + $Type + '] ' + $Message
+    $Entry | Out-File -FilePath $Path -Append
+}
+
+
+$FslogixFileShare = "\\$StorageAccountName.file.core.windows.net\$ShareName\"
+
+##############################################################
+#  Add Fslogix Settings
+##############################################################
+
+$Settings += @(
+    # Enables Fslogix profile containers: https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#enabled
+    [PSCustomObject]@{
+        Name         = 'Enabled'
+        Path         = 'HKLM:\SOFTWARE\Fslogix\Profiles'
+        PropertyType = 'DWord'
+        Value        = 1
+    },
+    # Deletes a local profile if it exists and matches the profile being loaded from VHD: https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#deletelocalprofilewhenvhdshouldapply
+    [PSCustomObject]@{
+        Name         = 'DeleteLocalProfileWhenVHDShouldApply'
+        Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+        PropertyType = 'DWord'
+        Value        = 1
+    },
+    # The folder created in the Fslogix fileshare will begin with the username instead of the SID: https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#flipflopprofiledirectoryname
+    [PSCustomObject]@{
+        Name         = 'FlipFlopProfileDirectoryName'
+        Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+        PropertyType = 'DWord'
+        Value        = 1
+    },
+    # # Loads FRXShell if there's a failure attaching to, or using an existing profile VHD(X): https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#preventloginwithfailure
+    # [PSCustomObject]@{
+    #         Name         = 'PreventLoginWithFailure'
+    #         Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+    #         PropertyType = 'DWord'
+    #         Value        = 1
+    # },
+    # # Loads FRXShell if it's determined a temp profile has been created: https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#preventloginwithtempprofile
+    # [PSCustomObject]@{
+    #         Name         = 'PreventLoginWithTempProfile'
+    #         Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+    #         PropertyType = 'DWord'
+    #         Value        = 1
+    # },
+    # List of file system locations to search for the user's profile VHD(X) file: https://docs.microsoft.com/en-us/fslogix/profile-container-configuration-reference#vhdlocations
+    [PSCustomObject]@{
+        Name         = 'VHDLocations'
+        Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+        PropertyType = 'MultiString'
+        Value        = $FslogixFileShare
+    },
+    [PSCustomObject]@{
+        Name         = 'VolumeType'
+        Path         = 'HKLM:\SOFTWARE\FSLogix\Profiles'
+        PropertyType = 'MultiString'
+        Value        = 'vhdx'
+    }
+)
+
+# Set registry settings
+foreach ($Setting in $Settings) {
+    # Create registry key(s) if necessary
+    if (!(Test-Path -Path $Setting.Path)) {
+            New-Item -Path $Setting.Path -Force
+    }
+
+    # Checks for existing registry setting
+    $Value = Get-ItemProperty -Path $Setting.Path -Name $Setting.Name -ErrorAction 'SilentlyContinue'
+    $LogOutputValue = 'Path: ' + $Setting.Path + ', Name: ' + $Setting.Name + ', PropertyType: ' + $Setting.PropertyType + ', Value: ' + $Setting.Value
+
+    # Creates the registry setting when it does not exist
+    if (!$Value) {
+            New-ItemProperty -Path $Setting.Path -Name $Setting.Name -PropertyType $Setting.PropertyType -Value $Setting.Value -Force
+            Write-Log -Message "Added registry setting: $LogOutputValue" -Type 'INFO'
+    }
+    # Updates the registry setting when it already exists
+    elseif ($Value.$($Setting.Name) -ne $Setting.Value) {
+            Set-ItemProperty -Path $Setting.Path -Name $Setting.Name -Value $Setting.Value -Force
+            Write-Log -Message "Updated registry setting: $LogOutputValue" -Type 'INFO'
+    }
+    # Writes log output when registry setting has the correct value
+    else {
+            Write-Log -Message "Registry setting exists with correct value: $LogOutputValue" -Type 'INFO'    
+    }
+    Start-Sleep -Seconds 1
+}
